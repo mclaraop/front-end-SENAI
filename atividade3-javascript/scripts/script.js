7e501d86000b7c4348f48af7a6f5ec63d84b3062
mclaraop <mclaraop@icloud.com>
2024-10-15 09:26:02 -0300

    atualização da atividade
    atualização do codigo da atividade

--- atividade3-javascript/scripts/script.js
+++ atividade3-javascript/scripts/script.js
@@ -1,3 +1,28 @@
-function listaLivros(){
-    
+// 1 - Faça uma listagem de 6 filmes de sua escolha com array e faça eles serem impressos no html
+
+function ex1(){
+    var filmes = ['O impossível', 'Meu porto seguro', 'É assim que acaba', 'A nova Cinderela']
+
+    filmes.forEach(listaFilme => {
+        document.querySelector("#respostaEx1").innerHTML += `
+        <p> ${listaFilme} </p>
+        `
+    });
+}
+
+// 2 - Fazer a tabuada com repetições recebendo os valores pelos inputs
+
+function ex2(){
+    let numero = Number (document.getElementById("inputEx2").value) 
+
+    let contador = 0
+    while(contador <= 10){
+        
+        contador++
+        let calculo = numero * contador
+
+        document.getElementById("respostaEx2").innerHTML += `
+            <p> ${numero} x ${contador} = ${calculo} </p>     
+        `
+    }
 }
